7524f514dddbb41c32c3f6df1ed1bd2e0c99489f
J Loo <junzhongloo@gmail.com>
2021-06-06 21:23:44 +1200

    DONE last digit sum partial fibonacci

--- algorithm_toolbox/week_2_assignment/7_Last_Digit_Partial_Fibonacco.py
+++ algorithm_toolbox/week_2_assignment/7_Last_Digit_Partial_Fibonacco.py
@@ -1,11 +1,8 @@
-# Last Digit of the Sum of Fibonacci Numbers
+# Last Digit of Sum of Partial Fibonacci
 # @author: Junzhong Loo
-# Date: 6/6/2021    
+# Date: 6/6/2021
 
 def main():
-    """
-    find last digit of sum of F(n)
-    """
     def calculate_pisano_period(m):
         """ 
         find mod m where start with 0,1 
@@ -22,26 +19,30 @@ def main():
             # found a period start with 01
             if (previous == 0 and current == 1):
                 return i + 1
-
-    def sum_of_last_digit_fibonacci(n):
+    
+    def last_digit_of_sum_of_partial_fibonacci(a, b):
+        """ 
+        start sum from point a
         """
-        last digit == mod 10
-        """   
-        pisano_period = calculate_pisano_period(10) # pisano_period == 60
+        # last digit == mod 10 == pisano_period of 60
+        pisano_period = calculate_pisano_period(10)
+        
+        b %= pisano_period
 
-        n %= 60
-
-        # sum all last digit 
+        # find previous and current from point a
         previous = 0
         current = 1
-        # at current
-        sum = 1
-        for _ in range(n - 1):
+        for _ in range(a - 1):
+            previous, current = current, previous + current
+        
+        sum = current
+        for _ in range(b - a):
             previous, current = current, previous + current
             sum += current
+        
         return sum % 10
 
-    print(sum_of_last_digit_fibonacci(3))
+    print(last_digit_of_sum_of_partial_fibonacci(10, 200))
 
 if __name__ == "__main__":
     main()
