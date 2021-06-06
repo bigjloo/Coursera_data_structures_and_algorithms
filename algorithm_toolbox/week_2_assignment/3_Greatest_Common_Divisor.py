4632956c6067f8d3def9f4f2c780676f59850f67
J Loo <junzhongloo@gmail.com>
2021-06-06 15:29:52 +1200

    DONE euclids algo for GCD

--- algorithm_toolbox/week_2_assignment/3_Greatest_Common_Divisor.py
+++ algorithm_toolbox/week_2_assignment/3_Greatest_Common_Divisor.py
@@ -2,4 +2,19 @@
 # @author: Junzhong Loo
 # Date: 5/6/2021    
 
+def main():
+    def efficient_GCD(a, b):
+        """ 
+        Using Euclids algorithn where GCD(a,b) == GCD(b, a%b). 
+        Break down problem into smaller problem
+        """
+        if b == 0:
+            return a
+        
+        return efficient_GCD(b, a % b)
 
+    print(efficient_GCD(320,24))
+
+
+if __name__ == "__main__":
+    main()
