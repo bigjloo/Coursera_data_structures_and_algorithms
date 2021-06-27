f2d69be2a3ceec7ecd423dc881b96c08c018dad6
J Loo <junzhongloo@gmail.com>
2021-06-27 23:04:19 +1200

    DONE car refuel

--- algorithm_toolbox/week_3_assignment/3_car_fueling/car_fueling.py
+++ algorithm_toolbox/week_3_assignment/3_car_fueling/car_fueling.py
@@ -3,39 +3,32 @@ import sys
 
 
 def compute_min_refills(distance, tank, stops):
-    # write your code here
-    """
-    check if trip is possible by comparing the distance of 
-    the two furthest stops from each other with full tank m
 
-    while d > 0:
-        find furthest stop we can go on full tank
-        distance -= stop distance from start
-        slice stops list
-        count += 1
-    """
-    # Find max distance between stops
-    # If tank is less than the max distance,
-    # goal is not reachable. Return -1
-    stops_copy = stops[:]
-    stops_copy.append(distance)
-    stops_sorted = sorted(stops_copy)
-    max_between_stops = stops_sorted[-1] - stops_sorted[-2]
-    if tank < max_between_stops:
-        return -1
-    
-    # CONTINUE
-    last_stop = 0
-    stops = 0
-    while distance > 0:
-        for i in range(len(stops)):
-            if tank < stops[i]:
-                last_stop += i-1
-                stops += 1
-                distance -= stops[i-1]
-                tank += stops[i-1]
-                stops = stops[i:]
-                break
+    n = len(stops)
+    # Add goal to stops
+    stops.append(distance)
+    # Add initial starting point to stops
+    stops = [0] + stops
+    # Initialize refill count and current index position in stops list
+    refill_count = 0
+    current_position = 0
+    # run while current position is not destination. destination = n + 1
+    while current_position <= n:
+        # Set our new starting point
+        last_position = current_position
+        # keep moving up one station until the next station refill not possible ie find max stops before refill
+        while current_position <= n and (stops[current_position + 1] - stops[last_position] <= tank):
+            current_position += 1
+        # If no stops for refill that satisfy tank max distance requirements
+        # return -1
+        if current_position == last_position:
+            return -1
+        # After finding best stop for refill and not at destination
+        # refill +1
+        if current_position <= n:
+            refill_count += 1
+
+    return refill_count
 
 if __name__ == '__main__':
     d, m, _, *stops = map(int, sys.stdin.read().split())
